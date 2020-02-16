76a2ca5f20dc1747386c5f29df89974d4c7f5713
steffenhansendev <mail@steffen-hansen.dk>
2020-02-16 16:17:20 +0100

    Removed unused old validateMoveWithIndexOfValidDice() in GameImpl

--- src/hotgammon/domain/GameImpl.java
+++ src/hotgammon/domain/GameImpl.java
@@ -152,35 +152,4 @@ public class GameImpl implements Game {
         introduceCheckersAt(Location.R6, 5, Color.RED);
         introduceCheckersAt(Location.R1, 2, Color.BLACK);
     }
-
-    private int validateMoveWithIndexOfValidDice(Location from, Location to) {
-        boolean isToOccupiedByOpponent =
-                checkerColor.get(from) != checkerColor.get(to) &&
-                        checkerColor.get(to) != Color.NONE;
-        if(isToOccupiedByOpponent) {
-            return -1;
-        }
-        boolean checkerIsNotOwnedByPlayerInTurn = playerInTurn != checkerColor.get(from);
-        if(checkerIsNotOwnedByPlayerInTurn){
-            return -1;
-        }
-        int signedDistanceOfMove = Location.distance(from, to);
-        boolean isBackwards =
-                playerInTurn == Color.BLACK && signedDistanceOfMove < 0 || playerInTurn == Color.RED && signedDistanceOfMove > 0;
-        if(isBackwards) {
-            return -1;
-        }
-        int absoluteDistanceOfMove = Math.abs(signedDistanceOfMove);
-        int indexOfValidDice = -1;
-        for(int i = 0; i < diceValuesLeft.length; i++) {
-            if(diceValuesLeft[i] == absoluteDistanceOfMove) {
-                indexOfValidDice = i;
-            }
-        }
-        boolean isDistanceNotAvailable = indexOfValidDice == -1;
-        if(isDistanceNotAvailable) {
-            return -1;
-        }
-        return indexOfValidDice;
-    }
 }
